edc21dc5fb9c50be62016a62c9974f8db27eaf40
asadbe0304 <asadbekaxmadkulov@gmail.com>
2023-03-15 15:53:33 +0500

    Profile theme add

--- src/pages/Profile/index.jsx
+++ src/pages/Profile/index.jsx
@@ -13,6 +13,8 @@ const index = () => {
             style={{
               backgroundImage:
                 "url('https://images.unsplash.com/photo-1499336315816-097655dcfbda?ixlib=rb-1.2.1&amp;ixid=eyJhcHBfaWQiOjEyMDd9&amp;auto=format&amp;fit=crop&amp;w=2710&amp;q=80')",
+              backgroundSize: "cover",
+              backgroundRepeat: "no-repeat",
             }}
           >
             <span
@@ -40,7 +42,7 @@ const index = () => {
             </svg>
           </div>
           <div className="container mx-auto px-4">
-            <div className="relative profile-inner ml-28 flex flex-col min-w-0 break-words bg-white w-9/12 mb-6 shadow-xl rounded-lg">
+            <div className="relative profile-inner ml-28 flex flex-col min-w-0 break-words w-9/12 mb-6 shadow-xl rounded-lg">
               <div className="px-6">
                 <div className="flex w-full flex-wrap justify-center items-center flex-col">
                   <div className="w-full profile-top ">
@@ -58,7 +60,7 @@ const index = () => {
                   <div className="w-full profile-bottom flex items-center flex-row-reverse justify-between ">
                     <div className="w-1/2 top-right flex items-end justify-end ">
                       <button
-                        className="bg-amber-500 active:bg-amber-600 uppercase  text-white font-bold  px-4 py-2 rounded outline-none focus:outline-none ease-linear transition-all text-sm duration-150"
+                        className="bg-amber-500 active:bg-amber-600 uppercase  nav-title font-bold  px-4 py-2 rounded outline-none focus:outline-none ease-linear transition-all text-sm duration-150"
                         type="button"
                       >
                         Connect
@@ -66,47 +68,41 @@ const index = () => {
                     </div>
                     <div className="w-1/2 top-left flex justify-between items-start">
                       <div className="p-3 text-center">
-                        <span className="text-xl font-bold block uppercase tracking-wide text-blueGray-600">
+                        <span className="text-xl font-bold block uppercase tracking-wide nav-title text-blueGray-600">
                           22
                         </span>
-                        <span className="text-sm text-blueGray-400">
-                          Achievement
-                        </span>
+                        <span className="text-sm nav-title">Achievement</span>
                       </div>
                       <div className="p-3 text-center">
-                        <span className="text-xl font-bold block uppercase tracking-wide text-blueGray-600">
+                        <span className="text-xl font-bold block uppercase tracking-wide nav-title text-blueGray-600">
                           10
                         </span>
-                        <span className="text-sm text-blueGray-400">
-                          Deggre
-                        </span>
+                        <span className="text-sm nav-title">Deggre</span>
                       </div>
                       <div className=" p-3 text-center">
-                        <span className="text-xl font-bold block uppercase tracking-wide text-blueGray-600">
+                        <span className="text-xl font-bold block uppercase tracking-wide nav-title text-blueGray-600">
                           89
                         </span>
-                        <span className="text-sm text-blueGray-400">
-                          Manager
-                        </span>
+                        <span className="text-sm nav-title">Manager</span>
                       </div>
                     </div>
                   </div>
                 </div>
                 <div className="text-center mt-12">
-                  <h3 className="text-4xl font-semibold leading-normal text-gray-700 mb-2">
+                  <h3 className="text-4xl font-semibold leading-normal nav-title mb-2">
                     {user}
                   </h3>
-                  <div className="text-sm leading-normal mt-0 mb-2 text-gray-400 font-bold uppercase">
+                  <div className="text-sm leading-normal mt-0 nav-title mb-nav-title font-bold uppercase">
                     WELCOME to system
                   </div>
-                  <div className="mb-2 text-gray-600 mt-10">
+                  <div className="mb-2 nav-title mt-10">
                     Creative Tim Officer
                   </div>
                 </div>
                 <div className="mt-10 py-10 border-t border-gray-200 text-center">
                   <div className="flex flex-wrap justify-center">
                     <div className="w-full lg:w-9/12 px-4">
-                      <p className="mb-4 text-lg leading-relaxed text-gray-700">
+                      <p className="mb-4 text-lg leading-relaxed nav-title">
                         An artist of considerable range, Jenna the name taken by
                         Melbourne-raised, Brooklyn-based Nick Murphy writes,
                         performs and records all of his own music, giving it a
