29385223a9c2ccd0c5bec193475fb943fb045d0a
asadbe0304 <asadbekaxmadkulov@gmail.com>
2023-03-16 21:05:27 +0500

    Ok

--- src/pages/Settings/user.jsx
+++ src/pages/Settings/user.jsx
@@ -4,9 +4,9 @@ import Tables from "./table";
 const user = () => {
   return (
     <>
-      <div className="flex items-center justify-start w-full settings-table">
+      <div className="flex items-center justify-start settings-table">
         <div className="w-full">
-          <div className=" lg:overflow-visible">
+          <div className="">
             <div className="flex lg:justify-between border-b-2 border-zinc-900 pb-1">
               <h2 className="text-2xl text-gray-500 font-bold">All Users</h2>
               <div className="text-center flex-auto">
