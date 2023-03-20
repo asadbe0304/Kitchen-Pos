327675ef29a90d6a95e0c02c7677e340b0fa894f
asadbe0304 <asadbekaxmadkulov@gmail.com>
2023-03-20 21:10:00 +0500

    Load work

--- src/components/loader/index.jsx
+++ src/components/loader/index.jsx
@@ -6,8 +6,12 @@ import { useState } from "react";
 const index = () => {
   const [load, setLoad] = useState(true);
   useEffect(() => {
-    window.addEventListener("load", setLoad(false));
+    window.addEventListener("load", ()=>{
+      return setLoad(false)
+    }, [load]);
+
   });
+  console.log(load);
   return (
     <>
       <div className={`load ${load ? "flex " : "hidden"}`}>
