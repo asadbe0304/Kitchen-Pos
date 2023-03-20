735a3afe637e0563b0a4e8bfea230ec359453e93
asadbe0304 <asadbekaxmadkulov@gmail.com>
2023-03-20 21:04:46 +0500

    Loader add

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import Router from "./Routes";
 import Navbar from "./components/Navbar";
 import Header from "./components/Header";
-
+import Load from "./components/loader";
 const App = () => {
   return (
     <>
+      <Load />
       <Header />
       <Navbar />
       <Router />
--- src/components/loader/index.jsx
+++ src/components/loader/index.jsx
@@ -1,9 +1,18 @@
-import React from 'react';
+import React from "react";
+import "./style.scss";
+import { useEffect } from "react";
+import { useState } from "react";
 
 const index = () => {
+  const [load, setLoad] = useState(true);
+  useEffect(() => {
+    window.addEventListener("load", setLoad(false));
+  });
   return (
     <>
-      
+      <div className={`load ${load ? "flex " : "hidden"}`}>
+        <div className="loader"></div>
+      </div>
     </>
   );
 };
