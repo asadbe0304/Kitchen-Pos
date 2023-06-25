1b8356a39f3548037dafd4f7553deb4b913bd177
asadbe0304 <asadbekaxmadkulov@gmail.com>
2023-06-25 15:26:49 +0500

    disable zoom

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,34 @@
 import Router from "./Routes";
 import Navbar from "./components/Navbar";
 import Header from "./components/Header";
+import { useEffect } from "react";
 
 const App = () => {
+  useEffect(() => {
+    const handleZoom = (event) => {
+      if (event.ctrlKey || event.metaKey) {
+        // Disable zoom on Ctrl/cmd + mouse wheel
+        event.preventDefault();
+      }
+    };
 
+    const disableZoom = () => {
+      // Disable zoom on the entire window
+      document.addEventListener("wheel", handleZoom, { passive: false });
+      document.addEventListener("keydown", handleZoom, { passive: false });
+    };
+
+    disableZoom(); // Apply the zoom disable when the component mounts
+
+    return () => {
+      // Clean up the zoom disable when the component unmounts
+      document.removeEventListener("wheel", handleZoom);
+      document.removeEventListener("keydown", handleZoom);
+    };
+  }, []);
   return (
     <>
-      <Header/>
+      <Header />
       <Navbar />
       <Router />
     </>
