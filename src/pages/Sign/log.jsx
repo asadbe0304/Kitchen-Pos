d3f6345db3a1eed161d34b657ed192b915c80fdf
asadbe0304 <asadbekaxmadkulov@gmail.com>
2023-06-29 03:14:09 +0500

    Login

--- src/pages/Sign/log.jsx
+++ src/pages/Sign/log.jsx
@@ -5,18 +5,19 @@ import Img from "./../../assets/img/logo.png";
 import { toast } from "react-toastify";
 
 const log = () => {
-  const [user, setUser] = useState();
-  const [pass, setPass] = useState();
-
+  const [user, setUser] = useState("Asadbek");
+  const [pass, setPass] = useState(12345678);
+  const [admin, setAdmin] = useState("Asadbek");
+  const [adminPass, setAdminPass] = useState(12345678);
   let userLocal = localStorage.setItem("user", user);
 
   const Navigate = useNavigate();
 
   const handles = () => {
-    if (!user.length == 0 && !pass.length == 0) {
+    if (user === admin && pass === adminPass) {
       Navigate("/home");
       toast.success(`You have successfully logged`, {
-        position: "bottom-center",
+        position: "bottom-left",
       });
     } else {
       toast.error("You have not logged in", {
@@ -50,6 +51,7 @@ const log = () => {
                 value={user}
                 onChange={(e) => setUser(e.target.value)}
                 required
+                placeholder="Asadbek "
                 className="mt-1 block w-full px-3 py-2 bg-white border border-slate-300 rounded-md text-sm shadow-sm placeholder-slate-400
       focus:outline-none focus:border-sky-500 focus:ring-1 focus:ring-sky-500
       disabled:bg-slate-50 disabled:text-slate-500 disabled:border-slate-200 disabled:shadow-none
@@ -67,6 +69,7 @@ const log = () => {
                 value={pass}
                 onChange={(e) => setPass(e.target.value)}
                 type="password"
+                placeholder="12345678"
                 className="mt-1 block w-full px-3 py-2 bg-white border border-slate-300 rounded-md text-sm shadow-sm placeholder-slate-400
       focus:outline-none focus:border-sky-500 focus:ring-1 focus:ring-sky-500
       disabled:bg-slate-50 disabled:text-slate-500 disabled:border-slate-200 disabled:shadow-none
